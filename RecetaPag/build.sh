1f6aec8da4b23b6efde20dce2de7edf33d5a366e
Sorath <santiagoacostaavila2905@gmail.com>
2025-05-05 20:00:08 -0500

    buiildsh

--- RecetaPag/build.sh
+++ RecetaPag/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+set -o errexit
+
 cd RecetaPag  # Entra a la carpeta que contiene manage.py
 pip install -r ../requirements.txt
-python manage.py collectstatic --noinput
+
+python manage.py collectstatic --no-input
 python manage.py migrate
