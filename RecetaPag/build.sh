b0d990b0a4256408d60aa13b1fac9ad260b8018f
Sorath <santiagoacostaavila2905@gmail.com>
2025-05-05 19:55:19 -0500

    commit

--- RecetaPag/build.sh
+++ RecetaPag/build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-set -o errexit
-
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
+cd RecetaPag  # Entra a la carpeta que contiene manage.py
+pip install -r ../requirements.txt
+python manage.py collectstatic --noinput
 python manage.py migrate
